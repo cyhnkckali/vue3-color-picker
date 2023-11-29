6ec43c0b2bf652b2ed670508ce9eccaf451f82dc
Ceyhun KÜÇÜKALİ <cyhnkckali@gmail.com>
2023-11-29 12:24:23 +0300

    Created

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup >
+import ColorPicker from "./components/ColorPicker.vue";
+</script>
+
+<template>
+    <div>
+
+        <ColorPicker />
+
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import ColorPicker from './components/ColorPicker.vue'
+
+export {ColorPicker}
