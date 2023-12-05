6527a1b9214327d2df2e665f381168a070bf4c3a
Ceyhun KÜÇÜKALİ <cyhnkckali@gmail.com>
2023-12-05 15:21:20 +0300

    Fixed

--- demo/src/main.js
+++
@@ -1,7 +0,0 @@
-import {createApp} from 'vue'
-import './style.css'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
-import '@cyhnkckali/vue3-color-picker/dist/style.css'
-createApp(App).mount('#app')
