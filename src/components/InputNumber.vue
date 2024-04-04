6bcc0132ddc5a0f2da477b7f6bb2e987baadf938
Pavel Kaskenov <p.kaskenov@gmail.com>
2024-04-05 01:05:30 +0300

    #17: Set InputNumber save on enter or focusout.

--- src/components/InputNumber.vue
+++ src/components/InputNumber.vue
@@ -1,50 +1,60 @@
 <template>
     <div class="ck-cp-input-content">
         <span class="ck-cp-input-label">{{ label }}</span>
-        <input type="number" :min="min" :max="max" :value="modelValue" @input="handleInputEvent($event)"
-            @keydown="handleRGBAOnKeydown" />
+        <input
+            type="number"
+            :min="min"
+            :max="max"
+            v-model="model"
+            @keydown="handleKeydown"
+            @focusout="handleFocusOut"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
-defineProps({
+import { computed, ref, watch } from 'vue';
+
+const props = defineProps({
     label: { default: '', type: String },
     modelValue: { default: 0, type: Number },
     min: { default: 0, type: Number },
     max: { default: 100, type: Number },
-})
+});
 
 const emits = defineEmits<{
-    (e: 'update:modelValue', value: number): void,
-    (e: 'onInput', value: Event): void,
-}>()
+    (e: 'update:modelValue', value: number): void
+}>();
 
+const internal = ref<number>(props.modelValue);
 
-const handleInputEvent = (e: Event) => {
-    emits('update:modelValue', parseInt((e.target as HTMLInputElement).value))
-    emits('onInput', e)
-}
+const model = computed({
+    get: () => internal.value,
+    set: (value) => internal.value = value
+});
 
-const handleRGBAOnKeydown = (event: KeyboardEvent) => {
-    if (
-        event.code == 'KeyE' ||
-        event.code == 'Equal' ||
-        event.code == 'NumpadSubtract' ||
-        event.code == 'Backslash' ||
-        event.code == 'Slash' ||
-        event.code == 'NumpadDecimal' ||
-        event.code == 'NumpadAdd'
-    ) {
-        event.preventDefault()
-    } else {
-        let max = parseInt((event.target as HTMLInputElement).max)
-        let val = parseInt((event.target as HTMLInputElement).value + event.key)
-        if (val > max) {
-            event.preventDefault()
-        }
-    }
-}
+watch(() => props.modelValue, (newValue, oldValue) => {
+    if (newValue !== oldValue) model.value = newValue;
+});
 
+const emitUpdateModelValue = (event: Event) => {
+    const updateModelAndEmitUpdateModelValue = (value: number) => {
+        model.value = value;
+        emits('update:modelValue', value);
+    };
+
+    const value = (event.target as HTMLInputElement).value;
+        
+    if (!value) return updateModelAndEmitUpdateModelValue(props.modelValue);
+    if (parseInt(value) > props.max) return updateModelAndEmitUpdateModelValue(props.max);
+    emits('update:modelValue', parseInt(value));
+};
+
+const handleKeydown = (event: KeyboardEvent) => {
+    const deprecatedCodes = ['KeyE', 'Equal', 'Equal', 'Backslash', 'Slash', 'NumpadDecimal', 'NumpadAdd'];
+    if (deprecatedCodes.includes(event.code)) return event.preventDefault();
+    if (event.code === 'Enter') emitUpdateModelValue(event);
+};
+
+const handleFocusOut = emitUpdateModelValue;
 </script>
-
-<style lang="scss" scoped></style>
